06062c9124ecfd7d0e99def91a1668fa27d749f3
e-rasvet <dyhanie.istiny@gmail.com>
2019-03-06 10:52:16 +0500

    Version file was updated

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php // $Id: version.php,v 1.2 2012/03/10 22:00:00 Igor Nikulin Exp $
 
-$plugin->version  = 2017073000; //2018033000; 2017073000; 2018043000;
+$plugin->version  = 2018040300; //2018033000; 2017073000; 2018043000;
 $plugin->requires = 2015051100;
 $plugin->release = '3.0.0.1';
 $plugin->maturity  = MATURITY_STABLE;
